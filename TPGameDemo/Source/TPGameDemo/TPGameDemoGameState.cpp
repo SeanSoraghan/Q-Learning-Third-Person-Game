cbd398527753387bb98b92d785d7772a1cb3f06c
seansoraghan <sasoraghan@googlemail.com>
2020-04-05 16:52:54 +0100

    Turret placement bugs - position off by half. Position value jittering on edge

--- TPGameDemo/Source/TPGameDemo/TPGameDemoGameState.cpp
+++ TPGameDemo/Source/TPGameDemo/TPGameDemoGameState.cpp
@@ -734,10 +734,8 @@ FRoomPositionPair ATPGameDemoGameState::GetRoomAndPositionForWorldXY(FVector2D w
         worldY -= roomLengthY / 2.0f;
     FIntPoint roomCoords = FIntPoint((int)(worldX / roomLengthX), (int)(worldY / roomLengthY));
     FVector2D roomWorldBottomLeft = FVector2D(roomCoords.X, roomCoords.Y) * FVector2D(roomLengthX, roomLengthY) - FVector2D(roomLengthX / 2.0f, roomLengthY / 2.0f);
-    ensure(worldXY.X >= roomWorldBottomLeft.X);
-    ensure(worldXY.Y >= roomWorldBottomLeft.Y);
-    int positionX = floor((worldXY.X - roomWorldBottomLeft.X) / (float)GridUnitLengthXCM);
-    int positionY = floor((worldXY.Y - roomWorldBottomLeft.Y) / (float)GridUnitLengthYCM);
+    int positionX = (int)((worldXY.X - roomWorldBottomLeft.X) / (float)GridUnitLengthXCM);
+    int positionY = (int)((worldXY.Y - roomWorldBottomLeft.Y) / (float)GridUnitLengthYCM);
     FIntPoint positionInRoom(positionX, positionY);
     return  { roomCoords, positionInRoom };
 }
